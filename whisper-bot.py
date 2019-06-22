549ef9bcf2df6f9c67d668e37e5e011b9d3afb08
ainis.antans <ainis.antans@gmail.com>
2019-06-23 00:32:46 +0300

    REFACTOR

--- whisper-bot.py
+++ whisper-bot.py
@@ -18,7 +18,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(message.content)
+    # print(message.content)
     if message.author == client.user:
         return
     guild = client.get_guild(SERVER_ID)
