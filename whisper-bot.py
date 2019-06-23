b5192838076a9a4ed7b0ad0ea8df9948b55243b5
ainis.antans <ainis.antans@gmail.com>
2019-06-23 12:33:26 +0300

    Refactored on_message() to call a function, instead of all the logic being inside the event

--- whisper-bot.py
+++ whisper-bot.py
@@ -2,9 +2,27 @@ import discord
 from discord.utils import get
 
 TOKEN = "NTkxNzQ4MjIxNjYwMDM3MTIx.XQ3h0g.tZeoZvrQtfo92VX4iPypChnPuzE"
+BETA_TOKEN = "NTkyMTE1NDM0NjQ4NjMzMzc3.XQ6ofQ.VyiD2DCfIGw4LeJHo7Frstu7YWw"
 client = discord.Client()
 TAG = "KSTA"
 
+async def on_ksta(message):
+    guild = message.guild
+    valid_channels = ["ksta"]
+    channel = message.channel
+
+    if str(message.channel) in valid_channels:
+        if str(message.content).endswith(TAG):
+            member = message.author
+            role = get(guild.roles, name=TAG)
+            if member != guild.owner:
+                await member.edit(nick=message.content)
+            if (role in member.roles) != 1:
+                await member.add_roles(role)
+            await channel.send(
+                member.mention + " твой ник был изменён на: `" + member.display_name + "` и тебе выдали роль " + role.name)
+        elif message.author != guild.owner:
+            await message.channel.purge(limit=1)
 
 @client.event
 async def on_ready():
@@ -19,22 +37,8 @@ async def on_message(message):
     # print(message.content)
     if message.author == client.user:
         return
-    guild = message.guild
-    valid_channels = ["ksta"]
-    channel = message.channel
+    await on_ksta(message)
 
-    if str(message.channel) in valid_channels:
-        if str(message.content).endswith(TAG):
-            member = message.author
-            role = get(guild.roles, name=TAG)
-            if message.author != guild.owner:
-                await member.edit(nick=message.content)
-            if (role in member.roles) != 1:
-                await member.add_roles(role)
-            await channel.send(
-                member.mention + " твой ник был изменён на: `" + member.display_name + "` и тебе выдали роль " + role.name)
-        elif message.author != guild.owner:
-            await message.channel.purge(limit=1)
 
 
 client.run(TOKEN)
