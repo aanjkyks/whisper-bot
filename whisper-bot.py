560d5a1b09aa14ec30d9824c17a71443a1244ccd
aanjkyks <ainis.antans@gmail.com>
2019-06-26 18:47:39 +0300

    Added a loop to check if role members have appropriate names on fortnite

--- whisper-bot.py
+++ whisper-bot.py
@@ -1,8 +1,10 @@
+import asyncio
 import urllib.parse
 
 from discord.ext import commands, tasks
 from discord.utils import get
 from fortnite_python import Fortnite
+from fortnite_python.exceptions import UnknownPlayerError
 from twitch import TwitchClient
 
 
@@ -16,6 +18,7 @@ TOKEN = file_reader("token.txt")
 BETA_TOKEN = file_reader("beta-token.txt")
 TWITCH_TOKEN = file_reader("twitch-token.txt")
 FORTNITE_API_KEY = file_reader("fortnite-token.txt")
+LOG_CHANNEL_NAME = "logs"
 
 bot = commands.Bot(command_prefix="")
 TAG = "KSTA"
@@ -35,8 +38,12 @@ async def on_ksta(message):
             if valid_name_length(message.content):
                 try:
                     player = fortnite.player(message.content)
-                except:
+                except UnknownPlayerError:
                     player = None
+                except:
+                    channel = get(message.guild.channels, name=LOG_CHANNEL_NAME)
+                    await channel.send("Error accessing fortnite-tracker API")
+                    return
                 if player is not None:
                     await member.add_roles(role)
                     await member.edit(nick=message.content)
@@ -54,14 +61,14 @@ async def on_ksta(message):
             await message.channel.purge(limit=1)
             await member.send(
                 "Для получения роли напиши свой никнейм Fortnite на канале " + message.channel.mention +
-                ". Пример: ``Ник KSTA``\nЕсли у тебя уже есть роль и ник, не пиши больше ничего :) Я всё равно всё "
-                "почистию и уберу :)")
+                ". Пример: ``Ник " "KSTA``\n" +
+                "Если у тебя уже есть роль и ник, не пиши больше ничего :) Я всё равно всё почистию и уберу :)")
 
 
 async def send_online():
     guilds = bot.guilds
     for guild in guilds:
-        channel = get(guild.channels, name="hello")
+        channel = get(guild.channels, name=LOG_CHANNEL_NAME)
         await channel.send("ONLINE")
 
 
@@ -101,19 +108,21 @@ async def on_member_remove(member):
 class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.notifier.start()
         self.offline = True
+        self.notifier.start()
+        self.check_role_members.start()
 
     def cog_unload(self):
         self.notifier.cancel()
+        self.check_role_members.cancel()
 
     @tasks.loop(minutes=10)
     async def notifier(self):
         for guild in self.bot.guilds:
             stream = client.streams.get_stream_by_user(TWITCH_CHANNEL.id)
-            if not stream is None:
+            if stream is not None:
                 if self.offline:
-                    channel = get(guild.channels, name="hello")
+                    channel = get(guild.channels, name=LOG_CHANNEL_NAME)
                     await send_stream_notification(stream, channel)
                     self.offline = False
             else:
@@ -121,7 +130,42 @@ class MyCog(commands.Cog):
 
     @notifier.before_loop
     async def before_notifier(self):
-        print("waiting...")
+        await self.bot.wait_until_ready()
+
+    @tasks.loop(hours=5)
+    async def check_role_members(self):
+        for guild in self.bot.guilds:
+            channel = get(guild.channels, name=LOG_CHANNEL_NAME)
+            await channel.send("Checking role members")
+            role = get(guild.roles, name=TAG)
+            for member in role.members:
+                if member.display_name == "BOT KSTA" or member == guild.owner:
+                    continue
+                if not TAG in member.display_name:
+                    await member.send("В нике отсутствует тег `" + TAG + "`. Я убрал тебе роль и ник на сервере.")
+                    await member.remove_roles(role)
+                    await member.edit(nick="")
+                    continue
+                try:
+                    player = fortnite.player(member.display_name)
+                except UnknownPlayerError:
+                    player = None
+                except:
+                    channel = get(guild.channels, name="stream-an")
+                    await channel.send("Error accessing fortnite-tracker API")
+                    return
+                if player is None:
+                    await member.remove_roles(role)
+                    await member.send("Не удалось найти игрока с ником `" + member.display_name +
+                                      "`. Я Убрал тебе роль и ник на сервере. Проверь, находит ли тебя здесь: "
+                                      "https://fortnitetracker.com/profile/pc/"
+                                      + urllib.parse.quote(member.display_name))
+                    await member.edit(nick="")
+                await asyncio.sleep(5)
+            await channel.send("role member check done")
+
+    @check_role_members.before_loop
+    async def before_notifier(self):
         await self.bot.wait_until_ready()
 
 
